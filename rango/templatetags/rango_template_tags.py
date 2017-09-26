504cd2b40dcce3b52528d9928e84ec7b3a97be54
Denis Savran <denis.savr@mail.ru>
2017-09-26 20:43:06 +0300

    Site now uses bootstrap

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -3,6 +3,15 @@ from rango.models import Category
 
 register = template.Library()
 
+
 @register.inclusion_tag('rango/cats.html')
 def get_category_list(cat=None):
     return {'cats': Category.objects.all(), 'act_cat': cat}
+
+
+@register.inclusion_tag('rango/nav_item.html')
+def dynamic_nav_item(current_url_name, url_name, a_tag_text):
+    if current_url_name == url_name:
+        return {'url_name': url_name, 'a_tag_text': a_tag_text, 'active': True}
+    else:
+        return {'url_name': url_name, 'a_tag_text': a_tag_text, 'active': False}
