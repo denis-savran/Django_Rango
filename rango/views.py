11c8f1ea4f377a6bf06d55f967dbb3155ab197c2
Denis Savran <denis.savr@mail.ru>
2017-10-14 18:27:51 +0300

    Added http decorators to views

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views.decorators.http import require_GET, require_safe
 
 from rango.documents import (CategoryDocument, PageDocument,
                              search_all_doc_types)
@@ -10,6 +12,7 @@ from rango.models import Category, Page, UserProfile
 from rango.utils import visitor_cookie_handler
 
 
+@require_safe
 def index(request):
     category_list = Category.get_most_viewed()
     pages_list = Page.get_most_viewed()
@@ -22,11 +25,13 @@ def index(request):
     return response
 
 
+@require_safe
 def about(request):
     context_dict = {'boldmessage': 'about'}
     return render(request, 'rango/about.html', context=context_dict)
 
 
+@require_safe
 def show_category(request, category_name_slug):
     context_dict = {}
     category = get_object_or_404(Category, slug=category_name_slug)
@@ -71,17 +76,16 @@ def add_page(request, category_name_slug):
 
 
 @login_required
+@require_GET
 def show_profile(request):
-    user = getattr(request, 'user', None)
-    try:
-        user_profile = UserProfile.objects.get(user=user)
-    except UserProfile.DoesNotExist:
-        user_profile = None
-    context_dict = {'user_profile': user_profile}
+    # user = request.user
+    # user_profile = get_object_or_404(UserProfile, user=user)
+    # context_dict = {'user_profile': user_profile}
 
-    return render(request, 'rango/profile.html', context=context_dict)
+    return render(request, 'rango/profile.html')
 
 
+@require_GET
 def search(request):
     context_dict = {}
 
